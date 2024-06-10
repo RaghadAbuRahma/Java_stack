874eb8772479fe8df4383fd4bcac5fd6c065b507
Raghad Aburahma <raghad.sa201@gmail.com>
2024-06-10 14:09:32 +0300

    first commit

---
+++ Java_Fund/first Java program/First.java
@@ -0,0 +1,14 @@
+public class First {
+    public static void main(String[] args){
+        System.out.println("My name is Raghad");
+
+        System.out.println("I am 23 years old");
+        System.out.println("My hometown is Ramallah");
+        }
+}
+
+
+
+
+
+
