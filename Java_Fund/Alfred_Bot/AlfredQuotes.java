923ca24ef7db0722e05394186f6f61a0b2586772
Raghad Aburahma <raghad.sa201@gmail.com>
2024-06-11 21:15:53 +0300

    Alfred_Bot

--- Java_Fund/Alfred_Bot/AlfredQuotes.java
+++ Java_Fund/Alfred_Bot/AlfredQuotes.java
@@ -15,14 +15,23 @@ public class AlfredQuotes {
         return "It is currently" + date;
     }
     
-    public String respondBeforeAlexis(String conversation) {
-        // YOUR CODE HERE
-        return "for snarky response return string";
+        public String respondBeforeAlexis(String conversation) {
+
+            if(conversation.indexOf("Alexis") > -1) {
+                return "She's really of no help. What can I get for you?";
+            }
+    
+            if (conversation.indexOf("Alfred") > -1) {
+                return "At your service, naturally. How may I be of assistance?";
+            }
+    
+            return "Right. And with that I shall retire.";
+        }
+        
     }
     
 	// NINJA BONUS
 	// See the specs to overload the guestGreeting method
         // SENSEI BONUS
         // Write your own AlfredQuote method using any of the String methods you have learned!
-}
 
--- Java_Fund/Alfred_Bot/AlfredTest.java
+++ Java_Fund/Alfred_Bot/AlfredTest.java
@@ -12,18 +12,17 @@ public class AlfredTest {
         String testGreeting = alfredBot.basicGreeting();
         String testGuestGreeting = alfredBot.guestGreeting("Beth Kane");
         System.out.println(testGuestGreeting);
-
         String testDateAnnouncement = alfredBot.dateAnnouncement();
         System.out.println(testDateAnnouncement);
+
         
-        String alexisTest = alfredBot.respondBeforeAlexis(
-                            "Alexis! Play some low-fi beats."
-                            );
-        String alfredTest = alfredBot.respondBeforeAlexis(
-            "I can't find my yo-yo. Maybe Alfred will know where it is.");
-        String notRelevantTest = alfredBot.respondBeforeAlexis(
-            "Maybe that's what Batman is about. Not winning. But failing.."
-        );
+        String alexisTest = alfredBot.respondBeforeAlexis("Alexis! Play some low-fi beats.");
+        System.out.println(alexisTest);
+        String alfredTest = alfredBot.respondBeforeAlexis("I can't find my yo-yo. Maybe Alfred will know where it is.");
+        System.out.println(alfredTest);
+        String notRelevantTest = alfredBot.respondBeforeAlexis("Maybe that's what Batman is about. Not winning. But failing..");
+        System.out.println(notRelevantTest);
+
         
         // Print the greetings to test.
         System.out.println(testGreeting);
