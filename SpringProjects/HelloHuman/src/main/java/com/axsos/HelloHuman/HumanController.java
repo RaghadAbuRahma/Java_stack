d5dadff6ff62c1c379a21b2f0df861197d874269
Raghad Aburahma <raghad.sa201@gmail.com>
2024-06-23 14:22:57 +0300

    HelloHuman

---
+++ SpringProjects/HelloHuman/src/main/java/com/axsos/HelloHuman/HumanController.java
@@ -0,0 +1,33 @@
+package com.axsos.HelloHuman;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class HumanController {
+	    @RequestMapping("")
+	    public String index(@RequestParam(value="name",  required=false) String name, @RequestParam(value="last_name",  required=false) String last_name) {
+	    	 
+	    	if (name != null && last_name != null) {
+	    		return "Hello :" + name + " " +  last_name; 
+	    		
+	    		}
+	    	if (name!= null) {
+	    		 return "Hello: " + name;
+	    	 }
+	    		 
+	        return "Hello human";
+	    	 
+
+	        
+		        
+	    }
+}
+
+	        
+
+
+
