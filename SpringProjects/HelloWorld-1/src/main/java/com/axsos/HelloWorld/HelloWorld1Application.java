75c9a53494885d1ad36d92962d18e26c92181770
Raghad Aburahma <raghad.sa201@gmail.com>
2024-06-23 12:05:37 +0300

    Daikichi

---
+++ SpringProjects/HelloWorld-1/src/main/java/com/axsos/HelloWorld/HelloWorld1Application.java
@@ -0,0 +1,22 @@
+package com.axsos.HelloWorld;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 2. Importing dependencies
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@SpringBootApplication
+// 1. Annotation
+@RestController
+public class HelloWorld1Application {
+        public static void main(String[] args) {
+                SpringApplication.run(HelloWorld1Application.class, args);
+        }
+        
+        // 1. Annotation
+        @RequestMapping("/")
+        // 3. Method that maps to the request route above
+        public String index() { // 3
+                return "Hello World";
+        }
+}
+
